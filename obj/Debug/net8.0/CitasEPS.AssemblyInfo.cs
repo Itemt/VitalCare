cc903ab89fc66dc5bfaa677866c5529dc69668e4
Cristian Ramos <crisrague1@gmail.com>
2025-05-30 03:10:13 -0500

    Mejora significativa en la interfaz de usuario de la página principal y el pie de página. Se actualizó la sección del héroe con un nuevo diseño, incluyendo elementos animados y un fondo mejorado. Se añadieron estadísticas interactivas y botones de acción optimizados. Además, se implementaron nuevos estilos CSS para el pie de página, incluyendo secciones de contacto y enlaces rápidos, así como un botón de desplazamiento hacia arriba. Se realizaron ajustes en la estructura HTML para mejorar la presentación y la experiencia del usuario.

--- obj/Debug/net8.0/CitasEPS.AssemblyInfo.cs
+++ obj/Debug/net8.0/CitasEPS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0f934dd7960e677c1db3ab15f657f8275f4be3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44a09e3f14797a312bb9445602200a5ecdc8402d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
