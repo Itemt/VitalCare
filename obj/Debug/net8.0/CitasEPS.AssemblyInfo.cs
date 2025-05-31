de5635f0da0060ec2d988ed205af14681cf1d337
Cristian Ramos <crisrague1@gmail.com>
2025-05-31 13:51:39 -0500

    Actualización de archivos binarios y ajustes en los activos estáticos. Se modificaron las versiones de los archivos CitasEPS.dll, CitasEPS.exe y CitasEPS.pdb. Además, se realizaron cambios en los encabezados de respuesta y la longitud del contenido en los archivos CSS, optimizando la configuración de los endpoints de activos estáticos. Se mejoraron las propiedades de los archivos CSS para reflejar la nueva estructura y se ajustaron las transiciones y efectos en el archivo enhanced-features.css.

--- obj/Debug/net8.0/CitasEPS.AssemblyInfo.cs
+++ obj/Debug/net8.0/CitasEPS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be3d3786d3922f7290d4ec8e0ebe633fb6a2f391")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae587e718e3f3f80103ea4cd4dfad351ee42a47f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
