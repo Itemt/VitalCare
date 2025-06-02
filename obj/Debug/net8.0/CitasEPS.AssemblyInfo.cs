40bc99ae88620ec5c1d5fca6e5880d80f698666e
Cristian Ramos <crisrague1@gmail.com>
2025-06-02 03:05:16 -0500

    Rediseño de las páginas de inicio de sesión y registro con nuevos estilos y estructura. Se implementaron formularios más modernos y responsivos, mejorando la experiencia del usuario. Se añadieron scripts para la gestión de validaciones y visibilidad de contraseñas. Además, se optimizaron los estilos CSS para una presentación más clara y accesible, incluyendo soporte para modo oscuro y mejoras en la interfaz de usuario.

--- obj/Debug/net8.0/CitasEPS.AssemblyInfo.cs
+++ obj/Debug/net8.0/CitasEPS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5d3a70bab30c508f86d31ab307fefcdd8c6d832")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a732f15f7e866b325f705006c73eb48dec45e2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
