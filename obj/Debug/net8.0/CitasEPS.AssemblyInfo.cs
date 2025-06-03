23afa3e03909e375ad9347198919b9b105a69bb8
Cristian Ramos <crisrague1@gmail.com>
2025-06-03 00:59:46 -0500

    Optimización de estilos y animaciones en Index.cshtml y Servicios.cshtml. Se mejoraron los efectos de los íconos flotantes, se simplificaron las animaciones y se ajustaron los colores para una mejor visibilidad. Además, se implementaron cambios en la estructura de las tarjetas de servicios para mejorar la presentación y la responsividad. Se actualizaron los encabezados de respuesta y se optimizaron los puntos finales de los recursos estáticos.

--- obj/Debug/net8.0/CitasEPS.AssemblyInfo.cs
+++ obj/Debug/net8.0/CitasEPS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be1a44ea90d9a866fb26261752b89025f8cb00e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+982983d74112125363ebaa0d9ed25be31587d007")]
 [assembly: System.Reflection.AssemblyProductAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
