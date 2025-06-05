db2736a648ec54d9c6d284ad3eba6357b87ff50f
Cristian Andrés Ramos Guerrero <crisrague1@gmail.com>
2025-06-04 19:17:03 -0500

    Mejoras en los estilos CSS de Team.cshtml, centradas en una mejor visibilidad y presentación de avatares e íconos tecnológicos tanto en modo claro como oscuro. Se añadieron estilos específicos para colores vibrantes en modo claro y se garantizó una visibilidad uniforme de los íconos. Se actualizaron los encabezados de respuesta de recursos estáticos para reflejar los cambios en las propiedades de integridad y huella digital.

--- obj/Debug/net8.0/CitasEPS.AssemblyInfo.cs
+++ obj/Debug/net8.0/CitasEPS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3813069f14fcb1cf811a06bdc8871a4f84d0c937")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c7393196639df8ebeb43010bda7d214712468e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
