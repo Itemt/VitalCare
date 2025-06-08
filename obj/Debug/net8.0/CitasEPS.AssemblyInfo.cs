b014f64dd80eabbe8eb739dd428a4de8e18077d0
Cristian Ramos <crisrague1@gmail.com>
2025-06-08 01:52:03 -0500

    Implementación de estilos específicos para botones de login y registro en _Layout.cshtml, asegurando consistencia visual y evitando rebotes en la interfaz. Se ajustaron dimensiones, colores y transiciones para mejorar la experiencia del usuario. Además, se excluyeron iconos del navbar en las reglas de las tarjetas de características para evitar conflictos de estilo. Se actualizaron los encabezados de respuesta de recursos estáticos para reflejar cambios en las fechas de modificación.

--- obj/Debug/net8.0/CitasEPS.AssemblyInfo.cs
+++ obj/Debug/net8.0/CitasEPS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a90e11413818594fac048302947c73f46917c8b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f7d514298af90e52e8c9a2561b8a18340a3e2da")]
 [assembly: System.Reflection.AssemblyProductAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
