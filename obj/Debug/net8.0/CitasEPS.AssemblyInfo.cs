867c7cbb940f6439a95593e9e7f3a5f83b2bed87
Cristian Ramos <crisrague1@gmail.com>
2025-06-09 12:47:26 -0500

    Actualización de archivos binarios y de depuración tras la compilación. Se modificaron las versiones de los archivos de ensamblado y se ajustaron los enlaces de origen para reflejar los cambios recientes en el código. Esto incluye la actualización de la información de versión y la corrección de archivos de caché relacionados.

--- obj/Debug/net8.0/CitasEPS.AssemblyInfo.cs
+++ obj/Debug/net8.0/CitasEPS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee4b34ed0a666395ad59c0b456f56b769d617953")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b3e1aedd759eb31624c23ca7c4d7a02e7ec9004")]
 [assembly: System.Reflection.AssemblyProductAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CitasEPS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
